ad0debf12d249e12e88c6a152b8d1259928cc1c9
Anonymous User <65959012+eleniconstantinou@users.noreply.github.com>
2020-11-24 17:14:14 -0500

    pls work

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MotionToSingleJusticeAppeals',
-      version='0.0.4',
+      version='0.0.5',
       description=('A docassemble extension.'),
       long_description='# docassemble.MotionToSingleJusticeAppeals\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\neconstantinou@su.suffolk.edu\r\n\r\n',
       long_description_content_type='text/markdown',
